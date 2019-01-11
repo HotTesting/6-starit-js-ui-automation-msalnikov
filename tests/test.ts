64b4f490bdbdbcd14388ec200bcabc8fe414a13f
Max Salnikov <salnikov.maksim@gmail.com>
2019-01-11 22:14:12 +0200

    Added fails test for CI

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,11 @@
-describe('WDIO', function () {
-    it('Should be alive', function () {
+import assert = require("assert");
+
+describe('Litecart main page', function () {
+    it('should be load Main page', function () {
+        const title = browser.getTitle();
         browser.url('/')
-        console.log('--Test passed!')
-    })
-})
+        browser.waitForVisible('.col-xs-auto .logotype .fakeCSS')
+        assert.strictEqual(title, 'My Store | Online Store')
+    });
+    
+});
