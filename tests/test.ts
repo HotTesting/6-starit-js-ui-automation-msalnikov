009cd0160558bfe12d004fd5b40b839b4cb24471
Max Salnikov <salnikov.maksim@gmail.com>
2019-01-12 18:47:19 +0200

    Test with wrong locator

--- tests/test.ts
+++ tests/test.ts
@@ -3,7 +3,7 @@ import assert = require("assert");
 describe('Litecart main page', function () {
     it('should be load Main page', function () {
         browser.url('/')
-        browser.waitForVisible('.col-xs-auto .logotype')
+        browser.waitForVisible('.col-xs-auto .logotype .fakeLocator')
         const title = browser.getTitle();
         assert.strictEqual(title, 'My Store | Online Store')
     });
