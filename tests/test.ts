8ab9ad80ae8f62da771f99888f2805819801b5f3
Max Salnikov <salnikov.maksim@gmail.com>
2019-01-12 18:58:50 +0200

    working test

--- tests/test.ts
+++ tests/test.ts
@@ -3,7 +3,7 @@ import assert = require("assert");
 describe('Litecart main page', function () {
     it('should be load Main page', function () {
         browser.url('/')
-        browser.waitForVisible('.col-xs-auto .logotype .fakeLocator')
+        browser.waitForVisible('.col-xs-auto .logotype')
         const title = browser.getTitle();
         assert.strictEqual(title, 'My Store | Online Store')
     });
