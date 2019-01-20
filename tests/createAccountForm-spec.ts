82f65389585e922117b4a0ee2c49044442ce3061
Max Salnikov <salnikov.maksim@gmail.com>
2019-01-21 01:17:50 +0200

    Added test for create new account

---
+++ tests/createAccountForm-spec.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai"
+
+describe.only('Create Account form', function() {
+
+    beforeEach(function() {
+        browser.url('/create_account');
+        browser.pause(500);
+    });
+
+    it('should create new User Account', function() {
+        const firstName = '.form-control[name="firstname"]';
+        const lastName = '.form-control[name="lastname"]';
+        const country = 'select[name=country_code]';
+        const email = '.col-md-6 .form-control[name="email"]';
+        const password = '.col-md-6 .form-control[name="password"]';
+        const confirmedPassword = '.col-md-6 .form-control[name="confirmed_password"]';
+        const newsLetterCheckbox = '.checkbox [name="newsletter"]';
+        const createAccountBtn = '.btn[name="create_account"]';
+        const alertSuccess = '.alert-success';
+        const alertDanger = '.alert-danger';
+        function randomValue() {
+            return Math.random().toString(36).slice(2)
+        };
+        $(firstName).setValue(randomValue());
+        $(lastName).setValue(randomValue());
+        $(country).selectByValue('UA');
+        $(email).setValue(`${randomValue()}@email.com`);
+        $(password).setValue('qwerty');
+        $(confirmedPassword).setValue('qwerty');
+        $(newsLetterCheckbox).click();
+        expect($(newsLetterCheckbox).isSelected()).to.be.true;
+        $(createAccountBtn).click();
+        expect($(alertSuccess).isVisible()).to.be.true;
+        expect($(alertDanger).isVisible()).to.be.false;
+    });
+
+});
